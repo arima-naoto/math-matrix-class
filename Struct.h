5e3296fcf170c12306c88f093c85642a101c5dc7
arima-naoto <k023g1010@g.neec.ac.jp>
2024-04-08 18:54:56 +0900

    プロジェクト ファイルを追加します。

---
+++ Struct.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/// <summary>
+/// 二次元ベクトル
+/// </summary>
+struct Vector2 final
+{
+	float x;
+	float y;
+};
+
+/// <summary>
+/// 行列2x2
+/// </summary>
+struct Matrix2x2 final
+{
+	float m[2][2];
+};
+
